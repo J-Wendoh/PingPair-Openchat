99b026bd26ca06677e377327129e58b0aa6ece96
J-Wendoh <joannewendoh@gmail.com>
2025-04-30 22:41:40 +0300

    Prepare PingPair Bot for deployment

--- src/model/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod types;
-pub mod state;
-
-// Re-export key functions and types
-pub use state::{get_user, create_user, update_user, create_pairing, complete_pairing, initialize_spotlight_countries};
-pub use types::{UserProfile, Pairing, PairingStatus, Session, CommandResponse, BotCommand}; 
